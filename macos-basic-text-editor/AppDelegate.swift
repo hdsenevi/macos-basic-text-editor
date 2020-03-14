794e1b434891f24532560abc9c7b9dde9ffa4929
Hdsenevi <shanaka36@gmail.com>
2020-03-14 21:31:53 +1100

    Initial Commit

---
+++ macos-basic-text-editor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  macos-basic-text-editor
+//
+//  Created by Shanaka Senevirathne on 14/3/20.
+//  Copyright © 2020 Shanaka Senevirathne. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macos-basic-text-editor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  macos-basic-text-editor
+//
+//  Created by Shanaka Senevirathne on 14/3/20.
+//  Copyright © 2020 Shanaka Senevirathne. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
